6db98355375069134830115e79302cf689dc51e5
fsdexter <fsdexter@gmail.com>
2021-07-21 17:59:34 +0000

    done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+	const [time, setTime] = useState(0);
+	const [timerOn, setTimerOn] = useState(false);
 
-//create your first component
-export function Home() {
+	useEffect(() => {
+		let interval = null;
+
+		if (timerOn) {
+			interval = setInterval(() => {
+				setTime(prevTime => prevTime + 10);
+			}, 10);
+		} else if (!timerOn) {
+			clearInterval(interval);
+		}
+
+		return () => clearInterval(interval);
+	}, [timerOn]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Timer">
+			<h2>Counter</h2>
+			<div id="display">
+				<FontAwesomeIcon icon={faClock} style={{ fontSize: "2em" }} />
+				<span>
+					{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+				</span>
+				<span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+				<span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+			</div>
+
+			<div id="buttons">
+				{!timerOn && time === 0 && (
+					<button onClick={() => setTimerOn(true)}>Start</button>
+				)}
+				{timerOn && (
+					<button onClick={() => setTimerOn(false)}>Stop</button>
+				)}
+				{!timerOn && time > 0 && (
+					<button onClick={() => setTime(0)}>Reset</button>
+				)}
+				{!timerOn && time > 0 && (
+					<button onClick={() => setTimerOn(true)}>Resume</button>
+				)}
+			</div>
 		</div>
 	);
 }
+
+export default App;
